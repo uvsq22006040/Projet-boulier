3d5609f0b770edf1035c16f89e57026a7185e312
Pierre-Axel RIVIERE <pierre-axel.riviere@ens.uvsq.fr>
2022-05-10 09:01:35 +0200

    FINIR A

--- boulier.py
+++ boulier.py
@@ -1,53 +1,47 @@
+
 import tkinter as tk
+import time
 
-############################
-# Définition des constantes
+C = 9
 
-# hauteur du canevas
 HAUTEUR = 600
-# largeur du canevas
-LARGEUR = 600
-# nombre de colonnes
-N = 9
+
+LARGEUR = 100 * C
+GAPX = 100
+GAPY = 80
+
+def init():
+    
+    canvas.create_rectangle(10, 10, LARGEUR , HAUTEUR, width=10)
+    canvas.create_line(10, 150, LARGEUR, 150, width=10)
+    
+    for j in range(4):
+        for i in range(C):
+            canvas.create_line(47+GAPX*i,10,47+GAPX*i,HAUTEUR, width=5)
+            canvas.create_oval(20+GAPX*i,200+GAPY*j,75+GAPX*i,255+GAPY*j, fill="black")
+        
+
+    for i in range(C):
+        canvas.create_oval(20+GAPX*i,20,75+GAPX*i,75, fill="black")
+
+def clic(event):
+    bille = canvas.find_closest(event.x,event.y,"oval")
+    billetags = canvas.gettags(bille)
+    colonnebille = int(billetags[1])
+    pass
 
 
-#######################
-# programme principal
-
-# définition des widgets
 racine = tk.Tk()
-racine.title("Soroban japonais")
-canvas = tk.Canvas(racine, width=LARGEUR, height=HAUTEUR)
-
-# Créer un ligne horizontal au quart de la hauteur de la fenêtre
-canvas.create_line(0, HAUTEUR / 4, LARGEUR, HAUTEUR / 4, fill="darkgrey", width=5)
-
-    # Créer N ligne verticales
-for i in range(N):
-    canvas.create_line(LARGEUR / (N + 1) * (i + 1), 0, LARGEUR / (N + 1) * (i + 1), HAUTEUR, fill="darkgrey", width=2)
-
-    # Créer des points blancs entre les lignes verticales
-for i in range(3, N, 3):
-    canvas.create_oval(LARGEUR / (N + 1) * (N - i + 0.5) - 5, HAUTEUR / 4 - 5, LARGEUR / (N + 1) * (N - i + 0.5) + 5, HAUTEUR / 4 + 5, fill="white")
-
-# Créer les boules, 1 au dessus de la ligne horizontale, 4 en dessous
-    for i in range(N):
-        for j in range(5):
-            if j == 0:
-                canvas.create_oval(
-                    LARGEUR / (N + 1) * (N - i) - 30,
-                    HAUTEUR / 8 - HAUTEUR / 16 - 10,
-                    LARGEUR / (N + 1) * (N - i) + 30,
-                    HAUTEUR / 8 - HAUTEUR / 16 + 30,
-                    fill="black"
-                )
-            else:
-                canvas.create_oval(
-                    LARGEUR / (N + 1) * (N - i) - 30,
-                    HAUTEUR / 4 + ((j + 1) * HAUTEUR / 8) - 10,
-                    LARGEUR / (N + 1) * (N - i) + 30,
-                    HAUTEUR / 4 + ((j + 1) * HAUTEUR / 8) + 30,
-                    fill="black"
-                )
+racine.title("boulier")
+canvas = tk.Canvas(racine, height = HAUTEUR, width = LARGEUR, bg="white")
 canvas.grid()
+
+canvas.create_rectangle(10, 10, LARGEUR , HAUTEUR, width=10)
+canvas.create_line(10, 150, LARGEUR, 150, width=10)
+
+
+
+init()
+
+
 racine.mainloop()
